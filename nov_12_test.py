bb96dedf0fc3b07309482cdda59640afef3400ac
Clark Whitehead <clark12341@gmail.com>
2023-12-05 17:16:14 -0800

    progress

---
+++ nov_12_test.py
@@ -0,0 +1,359 @@
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+
+from accelerate import Accelerator
+accelerator = Accelerator()
+
+from torch.utils.data import TensorDataset, DataLoader, DistributedSampler
+
+from llama import Tokenizer
+tokenizer = Tokenizer('./tokenizer.model')
+
+local_rank = accelerator.local_process_index
+
+# hyperparameters
+batch_size = 16 # how many independent sequences will we process in parallel?
+block_size = 64 # what is the maximum context length for predictions?
+max_iters = 100
+eval_interval = 100
+learning_rate = 1e-3
+# device = 'cuda' if torch.cuda.is_available() else 'cpu'
+device = accelerator.device
+eval_iters = 100
+n_embd = 128
+n_head = 4
+n_layer = 4
+dropout = 0.0
+# ------------
+
+torch.manual_seed(1337)
+
+with open('../data/shakespeare.txt', 'r', encoding='utf-8') as f:
+    text = f.read()
+
+# here are all the unique characters that occur in this text
+chars = sorted(list(set(text)))
+vocab_size = len(chars)
+# create a mapping from characters to integers
+stoi = { ch:i for i,ch in enumerate(chars) }
+itos = { i:ch for i,ch in enumerate(chars) }
+encode = lambda s: [stoi[c] for c in s] # encoder: take a string, output a list of integers
+decode = lambda l: ''.join([itos[i] for i in l]) # decoder: take a list of integers, output a string
+
+# Train and test splits
+data = torch.tensor(tokenizer.encode(text, False, False), dtype=torch.long)
+n = int(0.9*len(data)) # first 90% will be train, rest val
+train_data = data[:n]
+val_data = data[n:]
+
+# data loading
+def get_all_data(split):
+    data = train_data if split == 'train' else val_data
+    num_blocks = len(data) // block_size # number of total non-overlapping blocks
+
+    # Pre-allocate tensors for X and y
+    X = torch.zeros((num_blocks, block_size), dtype=torch.long, device=device)
+    y = torch.zeros((num_blocks, block_size), dtype=torch.long, device=device)
+    
+    for i in range(num_blocks -1):
+        start_ix = i * block_size
+        end_ix = start_ix + block_size
+        
+        # Fill in the X and y tensors
+        X[i] = data[start_ix:end_ix]
+        y[i] = data[start_ix+1:end_ix+1]
+    
+    return X, y
+
+# data loading
+def get_batch(split):
+    # generate a small batch of data of inputs x and targets y
+    data = train_data if split == 'train' else val_data
+    ix = torch.randint(len(data) - block_size, (batch_size,))
+    x = torch.stack([data[i:i+block_size] for i in ix])
+    y = torch.stack([data[i+1:i+block_size+1] for i in ix])
+    x, y = x.to(device), y.to(device)
+    return x, y
+
+@torch.no_grad()
+def estimate_loss():
+    out = {}
+    model.eval()
+    for split in ['train', 'val']:
+        losses = torch.zeros(eval_iters)
+        for k in range(eval_iters):
+            X, Y = get_batch(split)
+            logits, loss = model(X, start_pos=0, targets=Y)
+            losses[k] = loss.item()
+        out[split] = losses.mean()
+    model.train()
+    return out
+
+
+
+X_train, y_train = get_all_data('train')
+X_val, y_val = get_all_data('val')
+
+dataset_train = TensorDataset(X_train, y_train)
+dataloader_train = DataLoader(dataset_train, batch_size=batch_size, shuffle=True)
+
+dataset_val = TensorDataset(X_val, y_val)
+dataloader_val = DataLoader(dataset_val, batch_size=batch_size, shuffle=True)
+
+# data_train_iter = iter(dataloader_train)
+# data_val_iter = iter(dataloader_val)
+
+
+from typing import Optional, Tuple
+from dataclasses import dataclass
+import math
+
+import torch
+from torch import nn
+import torch.nn.functional as F
+
+# device = 'cuda' if torch.cuda.is_available() else 'cpu'
+device = accelerator.device
+
+
+@dataclass
+class ModelArgs:
+    dim: int = 128
+    n_layers: int = 4
+    n_heads: int = 4
+    vocab_size: int = 32_000
+    multiple_of: int = 256  # make SwiGLU hidden layer size multiple of large power of 2
+    norm_eps: float = 1e-5
+    max_batch_size: int = 16
+    max_seq_len: int = 64       
+
+
+class RMSNorm(torch.nn.Module):
+    def __init__(self, dim: int, eps: float = 1e-6):
+        super().__init__()
+        self.eps = eps
+        self.weight = nn.Parameter(torch.ones(dim))
+
+    def _norm(self, x):
+        return x * torch.rsqrt(x.pow(2).mean(-1, keepdim=True) + self.eps)
+
+    def forward(self, x):
+        output = self._norm(x.float()).type_as(x)
+        return output * self.weight
+
+
+def precompute_freqs_cis(dim: int, end: int, theta: float = 10000.0):
+    freqs = 1.0 / (theta ** (torch.arange(0, dim, 2)[: (dim // 2)].float() / dim))
+    t = torch.arange(end, device=freqs.device)  # type: ignore
+    freqs = torch.outer(t, freqs).float()  # type: ignore
+    freqs_cis = torch.polar(torch.ones_like(freqs), freqs)  # complex64
+    return freqs_cis
+
+
+def reshape_for_broadcast(freqs_cis: torch.Tensor, x: torch.Tensor):
+    ndim = x.ndim
+    assert 0 <= 1 < ndim
+    assert freqs_cis.shape == (x.shape[1], x.shape[-1])
+    shape = [d if i == 1 or i == ndim - 1 else 1 for i, d in enumerate(x.shape)]
+    return freqs_cis.view(*shape)
+
+
+def apply_rotary_emb(
+    xq: torch.Tensor,
+    xk: torch.Tensor,
+    freqs_cis: torch.Tensor,
+) -> Tuple[torch.Tensor, torch.Tensor]:
+    xq_ = torch.view_as_complex(xq.float().reshape(*xq.shape[:-1], -1, 2))
+    xk_ = torch.view_as_complex(xk.float().reshape(*xk.shape[:-1], -1, 2))
+    freqs_cis = reshape_for_broadcast(freqs_cis, xq_)
+    xq_out = torch.view_as_real(xq_ * freqs_cis).flatten(3)
+    xk_out = torch.view_as_real(xk_ * freqs_cis).flatten(3)
+    return xq_out.type_as(xq), xk_out.type_as(xk)
+
+
+class Attention(nn.Module):
+    def __init__(self, args: ModelArgs):
+        super().__init__()
+
+        self.n_heads = args.n_heads
+        self.head_dim = args.dim // args.n_heads
+
+        self.wq = nn.Linear(args.dim, args.n_heads * self.head_dim, bias=False)
+        self.wk = nn.Linear(args.dim, args.n_heads * self.head_dim, bias=False)
+        self.wv = nn.Linear(args.dim, args.n_heads * self.head_dim, bias=False)
+        self.wo = nn.Linear(args.n_heads * self.head_dim, args.dim, bias=False)
+
+    def forward(self, x: torch.Tensor, freqs_cis: torch.Tensor, mask: Optional[torch.Tensor]):
+        bsz, seqlen, _ = x.shape
+        xq, xk, xv = self.wq(x), self.wk(x), self.wv(x)
+
+        xq = xq.view(bsz, seqlen, self.n_heads, self.head_dim)
+        xk = xk.view(bsz, seqlen, self.n_heads, self.head_dim)
+        xv = xv.view(bsz, seqlen, self.n_heads, self.head_dim)
+
+        xq, xk = apply_rotary_emb(xq, xk, freqs_cis=freqs_cis)
+
+        keys = xk
+        values = xv
+
+        xq = xq.transpose(1, 2)
+        keys = keys.transpose(1, 2)
+        values = values.transpose(1, 2)
+        scores = torch.matmul(xq, keys.transpose(2, 3)) / math.sqrt(self.head_dim)
+        if mask is not None:
+            scores = scores + mask
+        scores = F.softmax(scores.float(), dim=-1).type_as(xq)
+        output = torch.matmul(scores, values)
+        output = output.transpose(1, 2).contiguous().view(bsz, seqlen, -1)
+
+        return self.wo(output)
+
+
+class FeedForward(nn.Module):
+    def __init__(
+        self,
+        dim: int,
+        hidden_dim: int,
+        multiple_of: int,
+    ):
+        super().__init__()
+        hidden_dim = int(2 * hidden_dim / 3)
+        hidden_dim = multiple_of * ((hidden_dim + multiple_of - 1) // multiple_of)
+
+        self.w1 = nn.Linear(dim, hidden_dim, bias=False)
+        self.w2 = nn.Linear(hidden_dim, dim, bias=False)
+        self.w3 = nn.Linear(dim, hidden_dim, bias=False)
+
+    def forward(self, x):
+        return self.w2(F.silu(self.w1(x)) * self.w3(x))
+
+
+class TransformerBlock(nn.Module):
+    def __init__(self, layer_id: int, args: ModelArgs):
+        super().__init__()
+        self.n_heads = args.n_heads
+        self.dim = args.dim
+        self.head_dim = args.dim // args.n_heads
+        self.attention = Attention(args)
+        self.feed_forward = FeedForward(
+            dim=args.dim, hidden_dim=4 * args.dim, multiple_of=args.multiple_of
+        )
+        self.layer_id = layer_id
+        self.attention_norm = RMSNorm(args.dim, eps=args.norm_eps)
+        self.ffn_norm = RMSNorm(args.dim, eps=args.norm_eps)
+
+    def forward(self, x: torch.Tensor, start_pos: int, freqs_cis: torch.Tensor, mask: Optional[torch.Tensor]):
+        h = x + self.attention.forward(self.attention_norm(x), freqs_cis, mask)
+        out = h + self.feed_forward.forward(self.ffn_norm(h))
+        return out
+
+
+class Transformer(nn.Module):
+    def __init__(self, params: ModelArgs):
+        super().__init__()
+        
+        self.params = params
+        self.vocab_size = params.vocab_size
+        self.n_layers = params.n_layers
+
+        self.tok_embeddings = nn.Embedding(params.vocab_size, params.dim)
+
+        self.layers = torch.nn.ModuleList()
+        for layer_id in range(params.n_layers):
+            self.layers.append(TransformerBlock(layer_id, params))
+
+        self.norm = RMSNorm(params.dim, eps=params.norm_eps)
+        self.output = nn.Linear(params.dim, params.vocab_size, bias=False)
+
+        self.freqs_cis = precompute_freqs_cis(
+            self.params.dim // self.params.n_heads, self.params.max_seq_len * 2
+        )
+
+    def forward(self, tokens: torch.Tensor, start_pos: int, targets=None):
+        _bsz, seqlen = tokens.shape
+        h = self.tok_embeddings(tokens)
+        self.freqs_cis = self.freqs_cis.to(h.device)
+        freqs_cis = self.freqs_cis[start_pos : start_pos + seqlen]
+
+        mask = None
+        if seqlen > 1:
+            mask = torch.full((1, 1, seqlen, seqlen), float("-inf"), device=tokens.device)
+            mask = torch.triu(mask, diagonal=start_pos + 1).type_as(h)
+
+        for layer in self.layers:
+            h = layer(h, start_pos, freqs_cis, mask)
+        h = self.norm(h)
+        logits = self.output(h)
+        
+        if targets is None:
+            loss = None
+        else:
+            B,T,C = logits.shape
+            logits = logits.view(B*T, C)
+            targets = targets.view(B*T)
+            loss = F.cross_entropy(logits, targets)
+        
+        return logits.float(), loss
+    
+    def generate(self, idx, max_new_tokens):
+        for _ in range(max_new_tokens):
+            
+            idx_cond = idx[:, -self.params.max_seq_len:]
+            
+            logits, loss = self(idx_cond, start_pos=0)
+            
+            logits = logits[:, -1, :]
+            
+            probs = F.softmax(logits, dim=-1)
+            
+            idx_next = torch.multinomial(probs, num_samples=1)
+            
+            idx = torch.cat((idx, idx_next), dim=1)
+        return idx
+
+
+args = ModelArgs()
+
+model = Transformer(args)
+model = model.to(device)
+# print the number of parameters in the model
+if local_rank == 0:
+    print(sum(p.numel() for p in model.parameters())/1e6, 'M parameters')
+
+# create a PyTorch optimizer
+optimizer = torch.optim.AdamW(model.parameters(), lr=1e-3)
+
+model, optimizer, dataloader_train = accelerator.prepare(
+    model, optimizer, dataloader_train
+)
+
+
+epochs = 5
+
+count = 0
+
+for epoch in range(epochs):
+    
+    for xb, yb in dataloader_train:
+        
+        count += 1
+        
+        # every once in a while evaluate the loss on train and val sets
+        if (count % eval_interval == 0 or count == max_iters - 1) and local_rank==0:
+            losses = estimate_loss()
+            print(f"step {count}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")          
+        
+        output_tensor, loss = model(xb, targets=yb, start_pos=0)
+        
+        optimizer.zero_grad()
+#         loss.backward()
+        accelerator.backward(loss)
+        optimizer.step()
+        
+        
+        
+# generate from the model
+context = torch.tensor(tokenizer.encode('H', False, False)).to(device).unsqueeze(0)
+if local_rank == 0:
+    print(tokenizer.decode(model.generate(context, max_new_tokens=500)[0].tolist()))        
